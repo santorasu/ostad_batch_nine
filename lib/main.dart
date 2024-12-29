8f92acfeef4ee8d092168b8832651f3c9de8e6a8
Santo Rasu <santorasu09.com>
2024-12-29 23:59:06 +0600

    This is assignment 1 solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,70 @@
+import 'package:ostad_batch_nine/teacher.dart';
+
+abstract class Role {
+  void displayRole();
+}
+
+class Person implements Role {
+  late String name;
+  late int age;
+  late String address;
+
+  Person(this.name, this.age, this.address);
+  @override
+  void displayRole() {
+    // print("This is override method");
+  }
+}
+
+class Student extends Person {
+  late String studentID;
+  late String grade;
+  late List<double> courseScores;
+
+  Student(
+    String name,
+    int age,
+    String address,
+    this.studentID,
+    this.grade,
+    this.courseScores,
+  ) : super(name, age, address);
+
+  @override
+  void displayRole() {
+    print("Role: Student");
+  }
+
+  double cal_Average() {
+    if (courseScores.isEmpty) {
+      return 0.0;
+    }
+    double total = 0;
+    for (int i = 0; i < courseScores.length; i++) {
+      total += courseScores[i];
+    }
+    double result = total / courseScores.length;
+    print(result);
+    return (result);
+  }
+}
+
 void main() {
-  print("This is assignment 1 demo for test");
+  Student st1 = Student("Rasu", 12, "Pabna", "4308", "3.5", [80, 90, 95]);
+  print("Student Information");
+  st1.displayRole();
+  print("Name: ${st1.name}");
+  print("Age: ${st1.age}");
+  print("Age: ${st1.address}");
+  print("Average Score: ${st1.cal_Average()}");
+  print("\n");
+  Teacher teacher1 =
+      Teacher("Farhan", 22, "Dhaka", "T4343", ['math', 'English', 'Bangla']);
+
+  print("Teacher Information");
+  teacher1.displayRole();
+  print("Name: ${teacher1.name}");
+  print("Name: ${teacher1.age}");
+  print("Name: ${teacher1.address}");
+  teacher1.displayCousesTought();
 }
---
+++ lib/teacher.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:ostad_batch_nine/main.dart';
+
+class Teacher extends Person {
+  late String teacherID;
+  late List<String> coursesTaught;
+
+  Teacher(
+      String name, int age, String address,this.teacherID, this.coursesTaught)
+      : super(name, age, address);
+
+  @override
+  void displayRole() {
+    print("Role: Teacher");
+  }
+
+  void displayCousesTought() {
+    print("Course Taught: ");
+    for (String course in coursesTaught) {
+      print("- $course");
+    }
+  }
+}
